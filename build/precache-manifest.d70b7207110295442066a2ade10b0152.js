05039dd7a5566cb1a58496682d5b8bf15ce6b122
David Prothero <david@prothero.com>
2019-06-03 18:51:26 -0700

    Add static build files

---
+++ build/precache-manifest.d70b7207110295442066a2ade10b0152.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a9c2ef97caef33e7baaa80d8006e845",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8c8d7d0e4098e8dbc825",
+    "url": "/static/css/main.fe28e4b9.chunk.css"
+  },
+  {
+    "revision": "87b6c56ea6df5187fcfc",
+    "url": "/static/js/2.6f748af3.chunk.js"
+  },
+  {
+    "revision": "8c8d7d0e4098e8dbc825",
+    "url": "/static/js/main.4754b526.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
